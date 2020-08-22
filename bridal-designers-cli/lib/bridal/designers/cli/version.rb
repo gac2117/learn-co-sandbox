2cd65b54f085c210a75e33e9045dcc127d71df2c
Grace A. Choi <oiograceoio@gmail.com>
2020-08-22 23:06:03 +0000

    Automatically backed up by Learn

--- bridal-designers-cli/lib/bridal/designers/cli/version.rb
+++
@@ -1,7 +0,0 @@
-module Bridal
-  module Designers
-    module Cli
-      VERSION = "0.1.0"
-    end
-  end
-end
--- bridal_designers/lib/bridal_designers.rb
+++
@@ -1,6 +0,0 @@
-require "bridal_designers/version"
-
-module BridalDesigners
-  class Error < StandardError; end
-  # Your code goes here...
-end
